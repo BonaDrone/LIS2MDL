c771d8e60a650b117e81b0d66764b652df7080b9
simondlevy <simon.d.levy@gmail.com>
2018-09-05 16:41:35 -0400

    Cleanup

--- src/LIS2MDL.h
+++ src/LIS2MDL.h
@@ -67,7 +67,7 @@ class LIS2MDL
 
     private:
 
-        //Register map for LIS2MDL'
+        // Register map
         // http://www.st.com/content/ccc/resource/technical/document/datasheet/group3/29/13/d1/e0/9a/4d/4f/30/DM00395193/files/DM00395193.pdf/jcr:content/translations/en.DM00395193.pdf
         static const uint8_t OFFSET_X_REG_L         = 0x45;
         static const uint8_t OFFSET_X_REG_H         = 0x46;
